5a4c7fcc2bb056c01f45ba9c99e2b99f7f220c81
sin12559 <134015898+sin12559@users.noreply.github.com>
2023-05-25 18:49:36 -0400

    changed copyright date

--- src/NewClass.java
+++ src/NewClass.java
@@ -1,6 +1,7 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Sagandeep
  */
 
 /**
@@ -24,7 +25,7 @@ public class StudentArray {
         Student[] students = new Student[5];
 
         // Populating the array with classmates' names
-        students[0] = new Student("Johnny");
+        students[0] = new Student("John");
         students[1] = new Student("Emily");
         students[2] = new Student("Michael");
         students[3] = new Student("Sarah");
